e1394a5e87e497a66f9c36450c5eac4ed7b4e924
Arun N <arunn5392@gmail.com>
2024-03-23 11:23:52 +0530

    counter

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Count from './Count'
+
+function App() {
+  return (
+    <div>
+
+
+
+<Count/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Count.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+
+function Count() {
+
+const [increment,setIncrement]=useState(0)
+const [decrement,setdecrement]=useState(0)
+
+const btn=()=>{
+    setIncrement(increment+1)
+
+}
+
+const btns=()=>{
+setdecrement(decrement-1)
+}
+
+  return (
+    <>
+    <div className="b">
+    <h1 className='blu'  >{increment}</h1>
+
+<br />
+    <button className='btn'  onClick={btn} >Count</button>
+
+    <br /><br />
+    
+        
+    <h1 className='blu' >{decrement}</h1>
+<br />
+
+    <button className='btnd'  onClick={btns} >Count</button>
+
+    </div>
+    
+    </>
+  )
+}
+
+export default Count
